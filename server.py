b93063ee660c5bc2663864bf541f3640da93dcd2
samTop123 <barsam618@gmail.com>
2026-01-03 00:16:40 +0200

    encrytion and decryption chat version

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
+import ssl
 import socket
 import constants
+import generating_key
+import info_for_signature
 from threading import Thread
 
 def create_server() -> socket.socket:
     return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def handle_client(client_socket: socket.socket, other_client_socket: socket.socket) -> None:
+def handle_client(client_socket: ssl.SSLSocket, other_client_socket: ssl.SSLSocket) -> None:
     while True:
         try:
             msg = client_socket.recv(constants.BUFFER_SIZE)
-            if msg == "[quit]":
+            if msg == b"[quit]":
+                client_socket.shutdown(socket.SHUT_RDWR)
                 client_socket.close()
-                other_client_socket.close()
                 break
             if msg:
                 other_client_socket.send(msg)
@@ -19,22 +22,35 @@ def handle_client(client_socket: socket.socket, other_client_socket: socket.sock
             print("The connection was lost !")
             break
 
+def secure_server_socket(server_socket : socket.socket) -> ssl.SSLSocket:
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    
+    if not generating_key.files_are_init():
+        generating_key.create_key_and_certificate()
+
+    context.load_cert_chain(certfile=info_for_signature.SERVER_CERT_NAME, keyfile=info_for_signature.SERVER_KEY_NAME_PRIVATE)
+
+    secured_server = context.wrap_socket(server_socket, server_side=True)
+
+    return secured_server
+
 if __name__ == "__main__":
     server = create_server()
 
     server.bind((constants.IP, constants.PORT))
+
     print("Waiting for clients...")
 
     server.listen(constants.MAX_CONNECTIONS)
-    
-    conn1, addr1 = server.accept()
+    secured_server = secure_server_socket(server)
+    conn1, addr1 = secured_server.accept()
 
     print("User 1 : ")
     print(addr1)
 
     conn1.send("Hello User 1".encode("utf-8"))
 
-    conn2, addr2 = server.accept()
+    conn2, addr2 = secured_server.accept()
 
     print("User 2 : ")
     print(addr2)
--- user.py
+++ user.py
@@ -1,19 +1,21 @@
 import tkinter as tk
 from tkinter import Tk, Text, Label, Button, END, NORMAL, DISABLED
 from tkinter.scrolledtext import ScrolledText
+import info_for_signature
 import client
 import constants
 import socket
 import threading
 import sys
+import ssl
 
-def end_program(root : Tk, client : socket.socket) -> None:
+def end_program(root : Tk, client : ssl.SSLSocket) -> None:
     client.send("[quit]".encode("utf-8"))
     root.destroy()
     client.close()
     sys.exit()
 
-def send_message(client_used : socket.socket) -> None:
+def send_message(client_used : ssl.SSLSocket) -> None:
     message = writing_text.get("1.0", END).strip().replace("\n", "")
     if message:
         client_used.send(message.encode("utf-8"))
@@ -22,25 +24,37 @@ def send_message(client_used : socket.socket) -> None:
         reading_text.config(state=DISABLED)
         writing_text.delete("1.0", END)
 
-def get_message(client : socket.socket) -> None:
+def append_text(text):
+    reading_text.config(state=NORMAL)
+    reading_text.insert(END, text)
+    reading_text.config(state=DISABLED)
+
+def get_message(client : ssl.SSLSocket) -> None:
     while True:
         try:
             msg = client.recv(constants.BUFFER_SIZE).decode("utf-8")
             if msg:
-                reading_text.config(state=NORMAL)
-                reading_text.insert(END, f"Other: {msg}\n")
-                reading_text.config(state=DISABLED)
+                root.after(0, append_text, f"Other: {msg}\n")
         except:
             print("The connection was lost !")
             break
 
-def initialize_message_area() -> None:
-    messages_thread = threading.Thread(target=get_message, args=(client_used,))
+def initialize_message_area(client_used : ssl.SSLSocket) -> None:
+    messages_thread = threading.Thread(target=get_message, args=(client_used,), daemon=True)
     messages_thread.start()
+
+def secure_client_socket(socket_client : socket.socket) -> ssl.SSLSocket:
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    context.load_verify_locations(info_for_signature.SERVER_CERT_NAME)
+
+    secured_client = context.wrap_socket(socket_client, server_hostname="localhost")
+
+    return secured_client
 
 if __name__ == "__main__":
     client_used = client.create_client()
-    client_used.connect((constants.IP, constants.PORT))
+    client_secured_used = secure_client_socket(client_used)
+    client_secured_used.connect((constants.IP, constants.PORT))
 
     root = Tk()
 
@@ -58,9 +72,9 @@ if __name__ == "__main__":
     readingLabel = Label(root, text = "Reading Area : ")
     readingLabel.config(font =("Courier", 14))
 
-    send_button = Button(root, text = "Send", command= lambda: send_message(client_used))
+    send_button = Button(root, text = "Send", command= lambda: send_message(client_secured_used))
 
-    exit_button = Button(root, text = "Exit", command = lambda: end_program(root, client_used))
+    exit_button = Button(root, text = "Exit", command = lambda: end_program(root, client_secured_used))
 
     writingLabel.pack()
     writing_text.pack()
@@ -71,6 +85,6 @@ if __name__ == "__main__":
     send_button.pack()
     exit_button.pack()
 
-    initialize_message_area()
+    initialize_message_area(client_secured_used)
 
     tk.mainloop()
